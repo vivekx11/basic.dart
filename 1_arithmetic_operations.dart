a6ee23e21532a3ec5bdb80316b133a9b8aa36b03
Vivek Sawji <168280120+vivekx11@users.noreply.github.com>
2026-01-20 18:34:08 +0530

    Create 1_arithmetic_operations.dart

---
+++ 1_arithmetic_operations.dart
@@ -0,0 +1,9 @@
+void main() {
+  int a = 10, b = 5;
+
+  print('Addition: $a + $b = ${a + b}');
+  print('Subtraction: $a - $b = ${a - b}');
+  print('Multiplication: $a * $b = ${a * b}');
+  print('Division: $a / $b = ${a / b}');
+  print('Modulus: $a % $b = ${a % b}');
+}
